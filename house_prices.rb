d2d459dfca230fcb1c0157c346be28396aa3a3c0
ainsleybc <ab.chang@me.com>
2017-09-25 22:14:44 +0100

    further refactoring into separate methods

--- house_prices.rb
+++ house_prices.rb
@@ -2,14 +2,16 @@ require_relative './house_prices_hash.rb'
 
 def convert_hash
   @house_prices.each do |k,v|
-    district = k[0]
-    ward = k[1]
-    trans = v[:transactions]
-    v[:count].times do |x|
-      puts "A house was sold in #{k[0]}, #{k[1]} for #{trans[x][0]} on #{trans[x][2]}"
-        end
+    district, ward, trans = k[0], k[1], v
+    print_trans(district, ward, trans)
     end
 end
 
+def print_trans(district, ward, trans)
+  tran = trans[:transactions]
+  trans[:count].times do |x|
+    puts "A house was sold in #{district}, #{ward} for #{tran[x][0]} on #{tran[x][2]}"
+  end
+end
 
 convert_hash
