cb17e8e34df5fa84d3048e5955bcf2033c8a9ba0
ainsleybc <ab.chang@me.com>
2017-09-26 09:42:42 +0100

    adding search method

--- house_prices.rb
+++ house_prices.rb
@@ -1,17 +1,22 @@
 require_relative './house_prices_hash.rb'
 
-def convert_hash
-  @house_prices.each do |k,v|
-    district, ward, all_trans = k[0], k[1], v
-    print_trans(district, ward, all_trans)
+def convert_hash(input_hash)
+  input_hash.each do |k,v|
+    district, ward, v_data = k[0], k[1], v
+    print_trans(district, ward, v_data)
     end
 end
 
-def print_trans(district, ward, all_trans)
-  each_tran = all_trans[:transactions]
-  all_trans[:count].times do |x|
+def print_trans(district, ward, v_data)
+  each_tran = v_data[:transactions]
+  v_data[:count].times do |x|
     puts "A house was sold in #{district}, #{ward} for #{each_tran[x][0]} on #{each_tran[x][2]}"
   end
 end
 
-convert_hash
+def search_hash(input_hash, string)
+  convert_hash(input_hash.select{ |k, v| k.include?(string) })
+end
+
+#convert_hash(@house_prices)
+#search_hash(@house_prices, "Sunderland")
